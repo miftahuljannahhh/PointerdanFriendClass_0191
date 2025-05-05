bd76b0b7b4e0d7dd9b150c30b335ace18f7e22c2
miftahuljannahhh <mj5416386@gmail.com>
2025-05-05 16:37:53 +0700

    comit5

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -11,18 +11,3 @@ class mahasiswa{
     }
 };
 
-int main (){
-
-    mahasisawa mhs; //Object mhs
-    mhs.nim = 5;
-    mhs.showNim(); //Member Access Operator
-
-    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
-    refMhs.nim = 2; //Member Access Operator
-    mhs.showNim();
-
-    mahasiswa *pMhs = &mhs; //pointer deference pMhs
-    pMhs->nim = 3; //Arrow Operator
-    pMhs->showNim();
-    return = 0;
-}
